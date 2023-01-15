d3ca57796e48ce7e69caf19894617d391bbf84d4
Code Sandbox <hello@codesandbox.io>
2023-01-15 22:50:28 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4ed396b2b8e02ce915660d7d2163ef6d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f04d9830009fb7c176a0",
+    "url": "https://pystr.github.io/csb-v97qvn/static/js/main.f04d9830.chunk.js"
+  },
+  {
+    "revision": "d7154097bc2c16f22fdf",
+    "url": "https://pystr.github.io/csb-v97qvn/static/js/1.d7154097.chunk.js"
+  },
+  {
+    "revision": "af84724ae17b8a3355ff",
+    "url": "https://pystr.github.io/csb-v97qvn/static/js/runtime~main.af84724a.js"
+  },
+  {
+    "revision": "b70a62c2932f0b9a1d063a6427ea5283",
+    "url": "https://pystr.github.io/csb-v97qvn/index.html"
+  }
+];
